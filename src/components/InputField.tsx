0c5eb2b8a42261e290a673e20f5fca3ddf6731cb
Ech0ke <aidas.zygas@gmail.com>
2023-02-05 11:41:46 +0200

    Create add todo logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import "./styles.css"
 
-export const InputField = () => {
+interface Props {
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent) => void
+}
+
+export const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input--box" />
-      <button className="input--submit" type="submit">
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input--box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input--submit" type="submit" onClick={handleAdd}>
         Go
       </button>
     </form>
